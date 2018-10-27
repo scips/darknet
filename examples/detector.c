3b21a0697622257b05bbe459f6cadea5e933e321
Sébastien Barbieri <sebastien.barbieri@gmail.com>
2018-10-27 08:40:50 +0200

    fix typo

--- examples/detector.c
+++ examples/detector.c
@@ -132,7 +132,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
         if (avg_loss < best_avg_loss) {
             best_avg_loss = avg_loss;
             char buff[256];
-            sprintf(buff, "%s/%s_best_avgs_loss.weights", backup_directory, base);
+            sprintf(buff, "%s/%s_best_avg_loss.weights", backup_directory, base);
             save_weights(net, buff);
             best_iteration = i;
         }
