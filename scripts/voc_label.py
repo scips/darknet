153f2766ed81091ca8be39862a721ffd27e38b63
Sébastien Barbieri <sebastien.barbieri@gmail.com>
2018-10-27 10:44:22 +0200

    add cat, buses, dog from pascal voc

--- scripts/voc_label.py
+++ scripts/voc_label.py
@@ -4,10 +4,11 @@ import os
 from os import listdir, getcwd
 from os.path import join
 
-sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
-
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
-
+#sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+# classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+# Focus on 3 classes only
+classes = ["bus", "cat", "dog"]
 
 def convert(size, box):
     dw = 1./(size[0])
